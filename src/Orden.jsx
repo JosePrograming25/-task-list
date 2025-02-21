efd1b1fcbcdeeef8e9a32a46655ba0f2304c2b48
jose <joseprograming25@gmail.com>
2025-02-21 02:35:42 -0400

    referencia a eslint con standard

--- src/Orden.jsx
+++ src/Orden.jsx
@@ -14,17 +14,17 @@ const Orden = ({ changeTareas: setTareas }) => {
     saveData('tareas', ordenadas)
   }
   return (
-        <div className="orden">
-            <span>Ordenar por: </span>
-            <select id="ordenarPor" onChange={ordenarTareas}>
-                <option value="noCompletadas">No completadas</option>
-                <option value="completadas">Completadas</option>
-                <option value="fechaDec">Fecha⏷</option>
-                <option value="fechaAcen">Fecha⏶</option>
-                <option value="alfabeticamenteDec"> Alfabéticamente⏷ </option>
-                <option value="alfabeticamenteAcen"> Alfabéticamente⏶</option>
-            </select>
-        </div>
+    <div className='orden'>
+      <span>Ordenar por: </span>
+      <select id='ordenarPor' onChange={ordenarTareas}>
+        <option value='noCompletadas'>No completadas</option>
+        <option value='completadas'>Completadas</option>
+        <option value='fechaDec'>Fecha⏷</option>
+        <option value='fechaAcen'>Fecha⏶</option>
+        <option value='alfabeticamenteDec'> Alfabéticamente⏷ </option>
+        <option value='alfabeticamenteAcen'> Alfabéticamente⏶</option>
+      </select>
+    </div>
 
   )
 }
